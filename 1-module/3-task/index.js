23507e7f837b1d6d884871b1886d5f08c3d7f7f5
Vadim Zhukov <vadim.zhukov@gmail.com>
2019-02-21 01:37:48 +0300

    Решение задач 2 и 3 1 модуля

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-
+    let arrayToSearch = [] = str.split(' ');
+    let min = 0;
+    let max = 0;
+    for (let i = 0; i < arrayToSearch.length; i++) {
+        if (!isNaN(+arrayToSearch[i])) {
+            min = (arrayToSearch[i] < min) ? arrayToSearch[i] : min;
+            max = (arrayToSearch[i] > max) ? arrayToSearch[i] : max;
+        }
+    }
+    return {min, max};
 }
-
