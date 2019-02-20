0d2b3a7bd4670a4854278a1e47592aceadfafbdd
Vadim Zhukov <vadim.zhukov@gmail.com>
2019-02-21 01:57:33 +0300

    Фикс 3 задачи с min и max

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,13 +6,13 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-    let arrayToSearch = [] = str.split(' ');
+    let arrayToSearch = [] = str.split(/,| /);
     let min = 0;
     let max = 0;
     for (let i = 0; i < arrayToSearch.length; i++) {
         if (!isNaN(+arrayToSearch[i])) {
-            min = (arrayToSearch[i] < min) ? arrayToSearch[i] : min;
-            max = (arrayToSearch[i] > max) ? arrayToSearch[i] : max;
+            min = (+arrayToSearch[i] < min) ? +arrayToSearch[i] : min;
+            max = (+arrayToSearch[i] > max) ? +arrayToSearch[i] : max;
         }
     }
     return {min, max};
