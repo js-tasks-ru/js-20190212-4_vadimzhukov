b2b6fd0be449bebd5f984bd5f27e3dda4dc937bc
Vadim Zhukov <vadim.zhukov@gmail.com>
2019-02-21 23:52:33 +0300

    решил задачу 1 модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @returns {Object}
  */
 function clone (obj) {
+    let cloneObj = {};
+    for (let key in obj) {
+        cloneObj[key] = obj[key];
+    }
+    return cloneObj;
 }
+
+let obj = { test: 1 };
+
+alert(clone(obj) !== obj); // это разные объекты
+alert(clone(obj).test === obj.test); // но содежат одни и те же поля
