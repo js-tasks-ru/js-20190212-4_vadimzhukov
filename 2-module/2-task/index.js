e124d5842b2a9f9ea3c432e935956ef5e7dee1d3
Zhukov Vadim <VZhukov@bell-main.bellintegrator.ru>
2019-02-28 10:26:17 +0300

    Решил задачу 2 модуля 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,48 @@
  * @returns {Object}
  */
 function find (obj, value) {
+    const paths = [];
+    for (let key in obj) {
+        if (obj[key] === value) {
+            paths.push(key);
+        }
+        if (typeof(obj[key]) === 'object') {
+            if (findInObject(obj[key], value, key) !== null ) {
+                paths.push(findInObject(obj[key], value, key));
+            }
+        }
+
+    }
+    return (paths.length == 1) ? paths[0] : paths;
 }
+
+function findInObject(obj, value, path) {
+    for (let k in obj) {
+        if (obj[k] === value) {
+            return path + '.' + k;
+        }
+        if (typeof(obj[k]) === 'object') {
+            path = path + '.' + k;
+            return findInObject(obj[k], value, path);
+        } else {
+            return null;
+        }
+    }
+}
+
+let obj = {
+    options: {
+        color: 'red'
+    },
+    style: {
+            border: {
+                color: 'red'
+            },
+            border2: {
+                color: 'red'
+        }
+    }
+};
+// console.log(find(obj, 'red'));
+//
+// console.log(JSON.stringify(find(obj, 'red')));// !== JSON.stringify(['options.color', 'style.border.color']));
