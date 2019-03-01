35d8665419a0c8e2b7e534e067d84a1dacb11bb7
Zhukov Vadim <VZhukov@bell-main.bellintegrator.ru>
2019-03-01 21:55:02 +0300

    Наконец-то решение 2 задачи 2 модуля :) убрал отладочный объект obj

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -37,18 +37,18 @@ function findInObject(obj, value, path) {
     return result;
 }
 
-let obj = {
-    options: {
-        color: 'red',
-        backgroundColor: 'red',
-        borderColor: 'green'
-    },
-    style: {
-        border: {
-            color: 'red'
-        }
-    }
-};
+// let obj = {
+//     options: {
+//         color: 'red',
+//         backgroundColor: 'red',
+//         borderColor: 'green'
+//     },
+//     style: {
+//         border: {
+//             color: 'red'
+//         }
+//     }
+// };
 // console.log(find(obj, 'red')); //Тест1. Должно быть три значение, а вернуло только 2
 // console.log(find(obj, 'red2')); // по условию задачи "Если ничего не найденно, то функция должна вернуть null", а вернулся пустой массив
 // console.log(find(obj, 'green')); // Тест 3. Должен был вернуть options.green, а вернул пустой массив
