a1aa526ab6a8630a8241ac5c36875d55248c42f9
Vadim Zhukov <vadim.zhukov@gmail.com>
2019-03-03 18:56:29 +0300

    модуль 3 - задача 1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,19 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let result='';
+    for (let i in data) {
+        if (data[i].age <= age) {
+            if (!result) {
+                debugger;
+                result = `${data[i].name}, ${data[i].balance}`;
+            } else {
+                result += `\n${data[i].name}, ${data[i].balance}`;
+            }
+        }
+
+    }
+    return result;
 }
 
 
