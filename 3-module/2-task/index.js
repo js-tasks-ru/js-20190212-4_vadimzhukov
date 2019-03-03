904de0ba046724250d966edc84c09edf353824b7
Vadim Zhukov <vadim.zhukov@gmail.com>
2019-03-03 21:52:30 +0300

    Модуль 3 задача 2 решена

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,38 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+    let fromCal = this.from;
+    let toCal = this.to;
+
+    return {
+        next() {
+            if (fromCal < toCal) {
+                if ((fromCal.getDay() == 5) || (fromCal.getDay() == 6)) {
+                    return {
+                        fromCal: fromCal.setDate(fromCal.getDate()+1),
+                        done: false,
+                        value: '['+('0' + fromCal.getDate()).slice(-2) + ']',
+
+                        };
+                    } else {
+
+                        return {
+                            fromCal: fromCal.setDate(fromCal.getDate()+1),
+                            done: false,
+                            value: ('0'+fromCal.getDate()).slice(-2),
+
+
+                        };
+                    }
+                } else {
+                    return {
+                        done: true
+                    }
+            }
+
+        }
+    }
+
 };
 
 
